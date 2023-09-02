bd20b93a4f732e7f84061c45b2e71e1d2d0076f5
Roman Kokhatskyi <roman.kokhatskyi@gmail.com>
2023-09-02 13:13:54 +0400

    #19 Implement hashing functions

--- src/hashing.js
+++ src/hashing.js
@@ -1,1 +1,11 @@
-// sample file
+const crypto = require('crypto');
+
+export function hash(string) {
+    const sha = crypto.createHash('sha3-256');
+    sha.update(string, 'utf-8');
+    return sha.digest('base64');
+}
+
+export function stringToHex(string) {
+    return Buffer.from(string, 'utf-8').toString('hex');
+}
