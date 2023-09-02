44ce99f9c6d1f9bfacd9cbc64c261f65f898c4d4
Roman Kokhatskyi <roman.kokhatskyi@gmail.com>
2023-09-02 14:09:45 +0400

    #19 Add error handling for hashing functions

--- src/hashing.js
+++ src/hashing.js
@@ -1,11 +1,26 @@
 const crypto = require('crypto');
 
-export function hash(string) {
-    const sha = crypto.createHash('sha3-256');
-    sha.update(string, 'utf-8');
-    return sha.digest('base64');
+function hash(string) {
+    try {
+        const sha = crypto.createHash('sha3-256');
+        sha.update(string, 'utf-8');
+        return sha.digest('base64');
+    } catch (error) {
+        console.error('Error while hashing:', error.message);
+        throw new Error('Failed to hash the string.');
+    }
 }
 
-export function stringToHex(string) {
-    return Buffer.from(string, 'utf-8').toString('hex');
+function stringToHex(string) {
+    try {
+        return Buffer.from(string, 'utf-8').toString('hex');
+    } catch (error) {
+        console.error('Error while converting to hex:', error.message);
+        throw new Error('Failed to convert the string to hexadecimal.');
+    }
 }
+
+module.exports = {
+    hash,
+    stringToHex,
+};
