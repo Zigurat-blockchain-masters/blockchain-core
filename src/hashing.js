0ce41346bf4b5366d5fee8f050a285cdd206e73a
Roman Kokhatskyi <roman.kokhatskyi@gmail.com>
2023-09-02 14:27:34 +0400

    #19 More JSDocs

--- src/hashing.js
+++ src/hashing.js
@@ -1,5 +1,11 @@
 const crypto = require('crypto');
 
+/**
+ * Hashes a string using SHA3-256 algorithm and returns the result in base64 encoding.
+ * @param {string} string - The string to be hashed.
+ * @returns {string} The base64-encoded hash of the input string.
+ * @throws {Error} Throws an error if hashing fails.
+ */
 function hash(string) {
     try {
         const sha = crypto.createHash('sha3-256');
@@ -10,6 +16,12 @@ function hash(string) {
     }
 }
 
+/**
+ * Converts a string to its hexadecimal representation.
+ * @param {string} string - The string to be converted.
+ * @returns {string} The hexadecimal representation of the input string.
+ * @throws {Error} Throws an error if conversion fails.
+ */
 function stringToHex(string) {
     try {
         return Buffer.from(string, 'utf-8').toString('hex');
