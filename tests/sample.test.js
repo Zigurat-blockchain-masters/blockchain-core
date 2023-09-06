21cda4091a71df86920184aaa1a485168bcfa362
Roman Kokhatskyi <roman.kokhatskyi@gmail.com>
2023-09-06 11:52:15 +0400

    #41 Remove sample test

--- tests/sample.test.js
+++
@@ -1,6 +0,0 @@
-describe('add function', () => {
-    it('should add two numbers correctly', () => {
-        const result = 2 + 3;
-        expect(result).toBe(5); // Expect the result to be 5 when adding 2 and 3
-    });
-});
